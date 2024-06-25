f9b36c5c5e5c10a7eb3e60c71606a230402ddee3
Agwathe <151617943+Agwathe@users.noreply.github.com>
2024-06-25 13:45:22 +0200

    création des comptes

--- ressources_sql/jeu_de_donnees_base.sql
+++ ressources_sql/jeu_de_donnees_base.sql
@@ -1,2 +1,11 @@
 INSERT INTO t_compte_cpt VALUES ('agathe@mail.fr', '666');
 INSERT INTO t_profil_pfl VALUES ('JAN', 'Agathe', 21, 'IMAC1', 'ETUDIANT', CURDATE());
+
+INSERT INTO t_compte_cpt VALUES ('sarah@mail.fr', 'bts');
+INSERT INTO t_profil_pfl VALUES ('BEAUJAULT', 'Sarah', 21, 'IMAC1', 'ETUDIANT', CURDATE());
+
+INSERT INTO t_compte_cpt VALUES ('lison@mail.fr', 'buck');
+INSERT INTO t_profil_pfl VALUES ('FORTIN', 'Lison', 21, 'IMAC1', 'ETUDIANT', CURDATE());
+
+INSERT INTO t_compte_cpt VALUES ('cherrier@mail.fr', 'tartiflette');
+INSERT INTO t_profil_pfl VALUES ('CHERRIER', 'Sylvain', NULL, NULL, 'PROFESSEUR', CURDATE());
