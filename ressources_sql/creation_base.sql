706cd0bd1387df81fcfa2870846a6f78591cbad4
Sarah.bjt <sarah.bjlt@gmail.com>
2024-06-25 10:16:08 +0200

    creation base de données

--- ressources_sql/creation_base.sql
+++ ressources_sql/creation_base.sql
@@ -9,10 +9,52 @@ CREATE TABLE t_compte_cpt (
 );
 
 CREATE TABLE t_profil_pfl (
-    pfl_id INT AUTO INCREMENT,
+    pfl_id INT AUTO_INCREMENT,
     pfl_nom VARCHAR(30),
     pfl_prenom VARCHAR(30),
     pfl_age INT,
-    pfl_classe classes,
-    CONSTRAINT pk_compte PRIMARY KEY (cpt_identifiant)
+    pfl_classe CLASSES,
+    pfl_statut STATUTS,
+    pfl_date_creation DATE,
+    cpt_identifiant VARCHAR(80),
+    CONSTRAINT pk_profil PRIMARY KEY (pfl_id),
+    CONSTRAINT fk_profil_compte FOREIGN KEY (cpt_identifiant) REFERENCES t_compte_cpt(cpt_identifiant)
+);
+
+CREATE TABLE t_cours_crs (
+    crs_id INT AUTO_INCREMENT,
+    crs_nom VARCHAR(80),
+    crs_semestre INT,
+    crs_date_creation DATE,
+    crs_descriptif VARCHAR(300),
+    CONSTRAINT pk_cours PRIMARY KEY (crs_id)
+);
+
+CREATE TABLE t_ressource_rsc (
+    rsc_id INT AUTO_INCREMENT,
+    rsc_nom VARCHAR(80),
+    rsc_lien VARCHAR(80),
+    rsc_type FICHIERS,
+    crs_id INT,
+    CONSTRAINT pk_ressource PRIMARY KEY (rsc_id),
+    CONSTRAINT fk_ressource_cours FOREIGN KEY (crs_id) REFERENCES t_cours_crs(crs_id)
+);
+
+CREATE TABLE t_exercice_exo (
+    exo_id INT AUTO_INCREMENT,
+    exo_nom VARCHAR(80),
+    exo_niveau INT,
+    exo_tps_passe INT,
+    exo_enoce VARCHAR(80),
+    crs_id INT,
+    CONSTRAINT pk_exercice PRIMARY KEY (exo_id),
+    CONSTRAINT fk_exercice_cours FOREIGN KEY (crs_id) REFERENCES t_cours_crs(crs_id)
+);
+
+CREATE TABLE t_education_educ (
+    cpt_identifiant VARCHAR(80),
+    crs_id INT,
+    CONSTRAINT pk_education PRIMARY KEY (cpt_identifiant,crs_id),
+    CONSTRAINT fk_education_compte FOREIGN KEY (cpt_identifiant) REFERENCES t_compte_cpt(cpt_identifiant),
+    CONSTRAINT fk_education_cours FOREIGN KEY (crs_id) REFERENCES t_cours_crs(crs_id)
 );
