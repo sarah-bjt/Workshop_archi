271f18a677307883753fc44739d2fff34e5c82a1
Lison <lisonfortin@icloud.com>
2024-06-26 11:24:04 +0200

    debut suivi_prof

--- server_pancakes.py
+++ server_pancakes.py
@@ -66,6 +66,15 @@ def suivi(util_id):
     #max = mycursor.fetchone()
     return render_template('suivi.html', recapitulatif=result, temps_total=temps_total, util_id=util_id) #max=max
 
+@app.route("/suivi_prof/<etud_id>/<prof_id>", methods=["GET", "POST", "PUT", "DELETE"])
+def suivi(util_id):
+    mycursor.execute("SELECT crs_nom, crs_semestre, SUM(rps_tps_passe) FROM t_cours_crs AS CRS JOIN t_exercice_exo AS EXO ON EXO.crs_id=CRS.crs_id JOIN t_reponse_rps AS RPS ON RPS.exo_id=EXO.exo_id GROUP BY CRS.crs_id ;")
+    result = mycursor.fetchall()
+    mycursor.execute("SELECT SUM(rps_tps_passe) FROM t_cours_crs AS CRS JOIN t_exercice_exo AS EXO ON EXO.crs_id=CRS.crs_id JOIN t_reponse_rps AS RPS ON RPS.exo_id=EXO.exo_id;")
+    temps_total = mycursor.fetchone()
+    #mycursor.execute("SELECT rps_date, SUM(rps_tps_passe) AS total_tps_passe, COUNT(DISTINCT exo_id) AS nombre_exercices FROM t_reponse_rps GROUP BY rps_date ORDER BY total_tps_passe DESC;")
+    #max = mycursor.fetchone()
+    return render_template('suivi_prof.html', recapitulatif=result, temps_total=temps_total, util_id=prof_id) #max=max
 
 @app.route("/devoir", methods=["GET"])
 def devoir():
