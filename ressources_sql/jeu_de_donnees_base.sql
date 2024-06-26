6faf2d83377b70afff4cf8d31b741be16592f7ca
sarah-bjt <151014995+sarah-bjt@users.noreply.github.com>
2024-06-26 11:09:49 +0200

    Update jeu_de_donnees_base.sql

--- ressources_sql/jeu_de_donnees_base.sql
+++ ressources_sql/jeu_de_donnees_base.sql
@@ -16,8 +16,8 @@ UPDATE t_cours_crs SET crs_semestre=1 WHERE crs_nom='PROGRAMATION WEB';
 INSERT INTO t_exercice_exo (exo_nom, exo_niveau, exo_enonce, crs_id) VALUES ('EXO 1', '1', 'met en gras avec la balise <b>','1');
 INSERT INTO t_exercice_exo (exo_nom, exo_niveau, exo_enonce, crs_id) VALUES ('EXO 2', '1', 'met en italique avec la balise <i>','1');
 
-INSERT INTO t_reponse_rps (rps_tps_passe, rps_correction, rps_commmentaire, exo_id) VALUES ('2', 'PAS CORRIGE', 'hyper facile', '2');
-INSERT INTO t_reponse_rps (rps_tps_passe, rps_correction, rps_commmentaire, exo_id) VALUES ('1', 'PAS CORRIGE', 'un poil plus compliqué', '3');
+INSERT INTO t_reponse_rps (rps_tps_passe, rps_correction, rps_commentaire, exo_id) VALUES ('2', 'PAS CORRIGE', 'hyper facile', '2');
+INSERT INTO t_reponse_rps (rps_tps_passe, rps_correction, rps_commentaire, exo_id) VALUES ('1', 'PAS CORRIGE', 'un poil plus compliqué', '3');
 
 INSERT INTO t_cours_crs (crs_nom,crs_semestre) VALUE ('ARCHITECTURE ALGO','2');
 INSERT INTO t_exercice_exo (exo_nom, exo_niveau, exo_enonce, crs_id) VALUES ('EXO 1', '1', 'Liste des profs qui dont le prénom est Pascale. Liste des prénoms. Liste des prénoms triés. Comment n’avoir qu’une seule fois chaque prénom ?','2');
