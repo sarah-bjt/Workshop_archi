8250eab699984c9aa20d3c28003383ea0bbf65e5
Lison <lisonfortin@icloud.com>
2024-06-26 14:26:07 +0200

    fonctions model.py rennomées

--- model.py
+++ model.py
@@ -9,48 +9,48 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-def get_user_by_email(email):
+def compte_par_id(email):
     mycursor.execute("SELECT * FROM t_compte_cpt WHERE cpt_identifiant=%(id)s;", {'id': email})
     return mycursor.fetchone()
 
-def get_profile_by_id(user_id):
+def profil_par_id(user_id):
     mycursor.execute("SELECT * FROM t_profil_pfl WHERE cpt_identifiant=%(id)s;", {'id': user_id})
     return mycursor.fetchone()
 
-def update_profile(user_id, nom, prenom, age, classe):
+def modifier_profil(user_id, nom, prenom, age, classe):
     mycursor.execute(
         "UPDATE t_profil_pfl SET pfl_nom=%(nnom)s, pfl_prenom=%(nprenom)s, pfl_age=%(nage)s, pfl_classe=%(nclasse)s WHERE cpt_identifiant=%(id)s;",
         {'nnom': nom, 'nprenom': prenom, 'nage': age, 'nclasse': classe, 'id': user_id}
     )
     mydb.commit()
 
-def delete_profile(user_id):
+def supprimer_profil(user_id):
     mycursor.execute("DELETE FROM t_profil_pfl WHERE cpt_identifiant=%(id)s;", {'id': user_id})
     mydb.commit()
     mycursor.execute("DELETE FROM t_compte_cpt WHERE cpt_identifiant=%(id)s;", {'id': user_id})
     mydb.commit()
 
-def get_students():
+def recuperation_etudiants():
     mycursor.execute("SELECT * FROM t_profil_pfl WHERE pfl_statut='ETUDIANT';")
     return mycursor.fetchall()
 
-def get_course_summary():
+def recapitulatif_cours_temps():
     mycursor.execute(
         "SELECT crs_nom, crs_semestre, SUM(rps_tps_passe) FROM t_cours_crs AS CRS JOIN t_exercice_exo AS EXO ON EXO.crs_id=CRS.crs_id JOIN t_reponse_rps AS RPS ON RPS.exo_id=EXO.exo_id GROUP BY CRS.crs_id ;"
     )
     return mycursor.fetchall()
 
-def get_total_time():
+def temps_total():
     mycursor.execute(
         "SELECT SUM(rps_tps_passe) FROM t_cours_crs AS CRS JOIN t_exercice_exo AS EXO ON EXO.crs_id=CRS.crs_id JOIN t_reponse_rps AS RPS ON RPS.exo_id=EXO.exo_id;"
     )
     return mycursor.fetchone()
 
-def get_exercises():
+def recuperation_exercices():
     mycursor.execute("SELECT * FROM t_exercice_exo;")
     return mycursor.fetchall()
 
-def create_account(email, password, name, surname):
+def creation_compte_profil(email, password, name, surname):
     mycursor.execute("INSERT INTO t_compte_cpt VALUES (%(em)s, %(pa)s)", {'em': email, 'pa': password})
     mydb.commit()
     mycursor.execute(
--- server_pancakes.py
+++ server_pancakes.py
@@ -12,14 +12,14 @@ def connexion():
     if request.method == "POST":
         identifiant = request.form['email']
         password = request.form['password']
-        result = model.get_user_by_email(identifiant)
+        result = model.compte_par_id(identifiant)
         if result and password == result[1]:
             return redirect(url_for('profil', util_id=identifiant))
     return render_template('connexion.html')
 
 @app.route("/profil/<util_id>", methods=["GET", "POST"])
 def profil(util_id):
-    result = model.get_profile_by_id(util_id)
+    result = model.profil_par_id(util_id)
     if request.method == "POST":
         form_type = request.form['form_type']
         if form_type == "modifier":
@@ -27,32 +27,32 @@ def profil(util_id):
             nouveau_classe = request.form['classe']
             nouveau_nom = request.form['nom']
             nouveau_prenom = request.form['prenom']
-            model.update_profile(util_id, nouveau_nom, nouveau_prenom, nouveau_age, nouveau_classe)
-            result = model.get_profile_by_id(util_id)
+            model.modifier_profil(util_id, nouveau_nom, nouveau_prenom, nouveau_age, nouveau_classe)
+            result = model.profil_par_id(util_id)
         elif form_type == "supprimer":
-            model.delete_profile(util_id)
+            model.supprimer_profil(util_id)
             return redirect(url_for('accueil'))
     if result[5] == 'PROFESSEUR':
-        result2 = model.get_students()
+        result2 = model.recuperation_etudiants()
         return render_template('profil_prof.html', professeur=result, etudiants=result2, util_id=util_id)
     else:
         return render_template('profil.html', etudiants=result, util_id=util_id)
 
 @app.route("/suivi/<util_id>", methods=["GET", "POST", "PUT", "DELETE"])
 def suivi(util_id):
-    result = model.get_course_summary()
-    temps_total = model.get_total_time()
+    result = model.recapitulatif_cours_temps()
+    temps_total = model.temps_total()
     return render_template('suivi.html', recapitulatif=result, temps_total=temps_total, util_id=util_id)
 
 @app.route("/suivi_prof/<etud_id>/<prof_id>", methods=["GET", "POST", "PUT", "DELETE"])
 def suivi_prof(etud_id, prof_id):
-    result = model.get_course_summary()
-    temps_total = model.get_total_time()
+    result = model.recapitulatif_cours_temps()
+    temps_total = model.temps_total()
     return render_template('suivi_prof.html', recapitulatif=result, temps_total=temps_total, util_id=prof_id)
 
 @app.route("/devoir", methods=["GET"])
 def devoir():
-    result = model.get_exercises()
+    result = model.recuperation_exercices()
     return render_template('devoir.html', exos=result)
 
 @app.route("/deconnexion", methods=['GET', 'POST'])
@@ -69,6 +69,6 @@ def creation_compte():
         name = request.form['name']
         surname = request.form['surname']
         password = request.form['password']
-        model.create_account(email, password, name, surname)
+        model.creation_compte_profil(email, password, name, surname)
         return redirect(url_for('profil', util_id=email))
     return render_template('creation.html')
