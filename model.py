db9648e9143fba247a2f1ceab50406c38ee50be9
Lison <lisonfortin@icloud.com>
2024-06-27 14:35:15 +0200

    style css

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="Jejxav-mahrag-0kivko",
     database="PANCAKE_L"
 )
 mycursor = mydb.cursor()
